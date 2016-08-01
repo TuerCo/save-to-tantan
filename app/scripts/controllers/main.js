077eaa53a85e9f247c92207af75446e598be6736
Juan J. Chiw <jjchiw@gmail.com>
2016-08-01 22:02:28 +0200

    added pagination, fix search, version ff-0.0.101, chrome: 0.0.98

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,8 +4,15 @@
         model.isLoading = true;
         model.showLogin = true;
         model.drawers = [];
+        model.totalDrawers = 0;
+        model.currentPage = 1;
+        model.pagesNumber = [];
         model.user = {};
+        model.drawerSearchTerm = '';
+        model.itemsPerPage = 5;
+
         var tantanAppUrl =  'http://tantan.local:9000/';
+        //var tantanAppUrl =  'http://tantan.localhost.com/';
         var tantanUrlCore = 'http://tantan.local:8585/';
         var tantanDashboard = tantanAppUrl+'#!/dashboard/';
         var tantanDrawer = tantanAppUrl+'#!/dashboard/drawers/';
@@ -20,7 +27,7 @@
                 }
                 token = dbToken.token;
 
-                getDrawers('').then(function successCallback(response) {
+                _getDrawers(1, '').then(function successCallback(response) {
                   _.forEach(response.data, function(drawer) {
                     drawer.selected = false;
                     model.drawers.push(drawer);
@@ -56,29 +63,63 @@
             model.logout = logout;
             model.goToDashboard = goToDashboard;
             model.searchDrawers = searchDrawers;
-
+            model.getDrawers = getDrawers;
         }
 
-        function getDrawers(term){
+        function getPagination(page) {
+            var start = Math.floor((page + model.itemsPerPage) / model.itemsPerPage);
+            model.currentPage = page;
+            if((model.currentPage * model.itemsPerPage) < model.totalDrawers){
+                if(model.totalDrawers / model.itemsPerPage >= model.itemsPerPage){
+                    model.pagesNumber = _range(model.itemsPerPage, start);
+                } else {
+                    model.pagesNumber = _range(Math.ceil(model.totalDrawers / model.itemsPerPage), start);
+                }
+            }
+        }
+
+        function _range(a,b){
+            var c=[];
+            while(a--){
+                c[a]=a+b;
+            }
+            return c
+        };
+
+        function getDrawers(page, term){
+            _getDrawers(page, term).then(function successCallback(response) {
+                model.drawers.length = 0;
+                _.forEach(response.data, function(drawer) {
+                    drawer.selected = false;
+                    model.drawers.push(drawer);
+                });
+            });
+        }
+
+        function _getDrawers(page, term){
+             $http({
+                method: 'GET',
+                url: tantanUrlCore+'_api/drawers/my/total?term='+term,
+                headers : {
+                    'Authorization' : 'Token ' + token
+                }
+            }).then(function(response){
+                model.totalDrawers = response.data.total;
+                getPagination(page)
+            });
+
             return $http({
                 method: 'GET',
-                url: tantanUrlCore+'_api/drawers/my/0/20?term='+term,
+                url: tantanUrlCore+'_api/drawers/my/'+page+'/'+model.itemsPerPage+'?term='+term,
                 headers : {
                     'Authorization' : 'Token ' + token
                 }
             });
         }
 
-        var debSearchDrawers = _.debounce(getDrawers, 250, { 'trailing': true });
+        var debSearchDrawers =_.debounce(getDrawers, 250, { 'trailing': true });
         function searchDrawers(){
-
-            debSearchDrawers(model.drawerSearchTerm).then(function successCallback(response) {
-                model.drawers.length = 0;
-                model.drawers = response.data;
-            }, function errorCallback(response) {
-                chrome.extension.getBackgroundPage().console.log(response);
-                model.showLogin = true;
-            });
+            debSearchDrawers(1, model.drawerSearchTerm);
         }
 
         function addToDrawer(drawerId) {
@@ -88,14 +129,16 @@
                     return;
                 }
                 var drawerTemp = drawerId;
+
+                //marking the drawer as selected even if the grab has not finished
+                var selectedDrawer = _.find(model.drawers, { id: drawerTemp});
+                selectedDrawer.selected = true;
+
                 getMetas(tab.url).then(function(grab){
                     postGrabToDrawer(drawerTemp, grab.data).then(function(response){
                         var selectedDrawer = _.find(model.drawers, { id: drawerTemp});
                         selectedDrawer.selected = true;
-
-                        var timeoutID = setTimeout(function () {
-                            window.close();
-                        }, 1500);
+                        window.close();
                     }, function(response){
                         chrome.extension.getBackgroundPage().console.log(response);
                     });
